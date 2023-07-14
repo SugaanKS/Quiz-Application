1b9ca3cb5f29c33ef4dd4ce0038456b286a9a5a8
sugaan <sugaansakthivel@gmail.com>
2023-07-14 11:33:36 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, {useState} from 'react'
+import MainMenu from './Components/MainMenu';
+import Quiz from './Components/Quiz'
+import EndScreen from './Components/EndScreen'
+import { QuizContext } from './Helpers/Contexts';
+
+
+function App() {
+  const [gameState,setGameState]= useState("menu");
+  const[score, setScore] =useState(0)
+
+  return (
+    <div className="App">
+      <h1>Welcome to the Quiz Contest</h1>
+      <QuizContext.Provider value={{gameState,setGameState,score,setScore}}>
+      {gameState==="menu" && <MainMenu/>}
+      {gameState==="quiz" && <Quiz/>}
+      {gameState==="endScreen" && <EndScreen/>}
+      </QuizContext.Provider>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainMenu.js
@@ -0,0 +1,19 @@
+import React,{useContext} from 'react'
+import { QuizContext } from '../Helpers/Contexts'
+import '../App.css'
+import pics from './j1.png'
+
+
+function MainMenu() {
+  const {gameState,setGameState}=useContext(QuizContext)
+  return (
+    <div className="Menu">
+      <img src={pics}></img>
+      <br></br>
+      <br></br>
+      <button onClick={() => setGameState("quiz")}>Start Quiz</button>
+    </div>
+  )
+}
+
+export default MainMenu
